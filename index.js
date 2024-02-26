130218d272fcdc02f6c4a43dfaace097ae27f22b
Deepakshi-23U <124867045+Deepakshi-23U@users.noreply.github.com>
2024-02-26 11:43:59 +0530

    Add main files

---
+++ index.js
@@ -0,0 +1,9 @@
+/*const divElement1 = document.querySelector('.one'); 
+
+divElement1.addEventListener('click', function() {
+  window.location.href = 'noticeBoard.html';
+});*/
+
+function openPage(pageUrl){
+    window.open(pageUrl);
+}
